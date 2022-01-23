fd98fde64df65c1a8b910b6b68cdba095d0afe9e
Tara-js Taras-js <shev-taras@ya.ru>
2022-01-23 19:58:16 +0400

    ES6 #3 Template strings

--- learning.js
+++ learning.js
@@ -146,13 +146,73 @@
 // })
 // console.log(ulHeader);
 
-const liHeader = document.querySelector('.color');
-
-liHeader.addEventListener('click', function (){
-    this.style.color = 'red';
-    // this.style.borderColor = 'green';
-    // this.style.textTransform = 'lowerCase';
-    // this.style.textTransform = 'upperCase';
-    this.style.fontSize = '35pt';
-})
-console.log(liHeader)
+// const liHeader = document.querySelector('.color');
+//
+// liHeader.addEventListener('click', function (){
+//     this.style.color = 'red';
+//     // this.style.borderColor = 'green';
+//     // this.style.textTransform = 'lowerCase';
+//     // this.style.textTransform = 'upperCase';
+//     this.style.fontSize = '35pt';
+// })
+// console.log(liHeader)
+// Шаблонные строки
+// const car = {
+//     name: 'Tesla',
+//     year: 2012
+// }
+// const details = `Автомобиль ${car.name} выпущен с завода в ${car.year} году`;
+// console.log(details)
+// const newHtml = `<div>${car.name}</div> <div>${car.year}</div>`;
+// document.body.innerHTML= newHtml;
+// console.log(newHtml)
+// const cars = [
+//     {   name: 'Tesla',
+//         year: 2019
+//     },
+//     {   name: 'Lada',
+//         year: 2010
+//     },
+//     {   name: 'Lada',
+//         year: 2010
+//     },
+//     {   name: 'Lada',
+//         year: 2010
+//     },
+//     {   name: 'Lada',
+//         year: 2010
+//     }
+// ]
+// const htmlNew = `
+// <ul>
+// ${cars.map(car => `<li>${car.name}</li>`).join(' ')}
+// </ul>
+// `;
+// console.log(htmlNew)
+// document.body.innerHTML = htmlNew;
+// const car = {
+//     // name: 'Tesla',
+//     // year: 2012,
+//     info: ['electric', 'modern', 'functional']
+// };
+// const infoAdd = (infos) => {
+//    return`
+// <div>
+//     ${infos.map(info => `<div>${info}</div>`).join(' ')}
+// </div>
+// `;
+//
+// }
+// const newHtml = `
+// <ul>
+// ${car.name ? `<p>${car.name}</p>` : ''}
+//
+// ${car.year ? `<p>${car.year}</p>`: ''}
+// <p>${infoAdd(car.info)}</p>
+//
+//
+// </ul>
+//
+// `;
+// console.log(newHtml)
+// document.body.innerHTML = newHtml;
