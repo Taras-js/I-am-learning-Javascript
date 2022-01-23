8f0f104a3db5c62155cfbb00a77b0fc97fb62671
Tara-js Taras-js <shev-taras@ya.ru>
2022-01-23 22:17:56 +0400

    ES6 #4 Destructuring in Javascript

--- learning.js
+++ learning.js
@@ -15,11 +15,11 @@
 // let myVariable = 'Taras';
 // myHeading.textContent = `'Hello world!' ${myVariable}`; //здесь я решил поиграться со знаниями с прошлого курса - всплыли в голове
 // Все в JS является объектом и может храниться в перменной: строка, число, булевы значения, массивы, объекты
-/* Если комментарий в несколько 
-строк с разрывом то делаем так 
+/* Если комментарий в несколько
+строк с разрывом то делаем так
 в дургом случае как в примерах закоментированных сверху
 */
-// Операторы 
+// Операторы
 /* + оператор добавления или сложения  -* / вычитание, умножение и деление*/
 // let number1 = 6;
 // let number2 = 8;
@@ -201,7 +201,6 @@
 //     ${infos.map(info => `<div>${info}</div>`).join(' ')}
 // </div>
 // `;
-//
 // }
 // const newHtml = `
 // <ul>
@@ -209,10 +208,61 @@
 //
 // ${car.year ? `<p>${car.year}</p>`: ''}
 // <p>${infoAdd(car.info)}</p>
-//
-//
 // </ul>
-//
 // `;
 // console.log(newHtml)
 // document.body.innerHTML = newHtml;
+// Деструктуризация ES6
+let green = 'green';
+let red = 'red';
+[green, red] = [red, green];
+console.log({green, red})
+const car = {
+    name: 'Tesla',
+    year: 2012,
+    info: ['electric', 'modern', 'functional'],
+    date: {
+        width: '100mm',
+        length: '200mm'
+
+    }
+};
+const {name, year, info} = car;
+console.log({name, year, info})
+const {width: newWidth, length} = car.date;
+console.log({newWidth, length})
+const car = {
+    name: 'Tesla',
+    year: 2020,
+    info: ['electric', 'modern', 'functional'],
+    date: {
+        width: '100mm',
+        length: '200mm'
+
+    }
+};
+const {name, year = 'не известен'} = car;
+console.log({name,year})
+const cars = ['Nissan', 'automat', '2012', 'red'];
+ const [marka, kpp, year, color] = cars;
+ console.log({marka, kpp, year, color})
+const autoCar = 'Москвич, 1920 года, убитый в хлам, ездил дедушка, 100лет';
+const [brand, year, state, ...client] = autoCar.split(',');
+console.log({brand, year, state, client})
+const [owner, store] = client;
+console.log(owner, store)
+function converterRubles  (rubles)  {
+    const currency = {
+        EUR: rubles/85,
+        USD: rubles/74,
+        GBP: rubles/105
+            }
+            return currency;
+}
+const {EUR, USD, GBP} = converterRubles(10000);
+console.log({EUR, USD, GBP})
+function getTotal ({total = 2000, delivery = 0.1} = {}) {
+    return total + total * delivery;
+    }
+    const total = getTotal({total: 5000, delivery: 0.2});
+console.log(total)
