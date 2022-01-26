5ee64099c9def762e27ab562feb50e6a7cc5206e
Tara-js Taras-js <shev-taras@ya.ru>
2022-01-26 18:37:21 +0400

    Closure

--- closure.js
+++ closure.js
@@ -2,7 +2,7 @@
 const externalFunction = function (name){
     const externalLet = `${name} - Я Большая пребольшая функция попробуй замкни меня :)`;
 
-    const internalFunction = function () {
+     function internalFunction() {
         const internalLet = `${name} - Я маленькая премаленькая функция попробуй найди меня`;
         console.log('BigFunction >', externalLet);
         console.log('LittleFunction >', internalLet);
@@ -13,3 +13,39 @@ const externalFunction = function (name){
 
     const callInternalFunction = externalFunction('Taras');
     callInternalFunction()
+
+function helloPlayers (name) {
+    const lastName = `Bay ${name}`;
+
+    function hello () {
+       const firstName = `Hello ${name} `;
+        alert(firstName);
+        alert(lastName);
+    }
+        return hello;
+
+}
+const hey = helloPlayers('Taras');
+hey();
+
+function nameDurak (name) {
+    let durak = ', извините, но Вы дурак';
+   return function () {
+
+        return `${name + durak} `
+    }
+}
+const newDurak = nameDurak('Taras Vasilievich')
+newDurak()
+
+function stepCounter (firstName) {
+    let start = 0;
+    return function () {
+        start++;
+        return `${firstName} сделал ${start} шагов`
+    }
+}
+const playerTwo = stepCounter('Taras')
+const playerFoo = stepCounter('Sacha')
+playerFoo();
+playerTwo();
