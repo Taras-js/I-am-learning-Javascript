4a4a93724edbf111edcfe0e815fbaefe9ab93d76
Tara-js Taras-js <shev-taras@ya.ru>
2022-01-26 14:23:56 +0400

    Functions expression and declaration

---
+++ function_expressions_declaration.js
@@ -0,0 +1,12 @@
+declarationFunction();
+function declarationFunction () {
+    console.log('Ой, ёёёёёёёёёёёёёёёёёёёё')
+}
+
+//expressionFunction()
+const expressionFunction = function () {
+    console.log('Ай, яяяяяяяяяяяяяяяяяяяяя')
+}
+expressionFunction()
+console.log(gerT)
+const gerT = 100;
